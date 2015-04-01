dbff36814438354556a872800a8e2562be41c507
Alexander Logunov <unlovedru@gmail.com>
2015-04-01 19:28:02 +0300

    dataase recipe fix

--- recipes/_database.rb
+++ recipes/_database.rb
@@ -1,4 +1,4 @@
 def recipe_database
-  file 'config/database.yml.example', File.read("#{@static_files_path}/database.yml").gsub('db_name', @application_name.downcase)
-  file 'config/database.yml', File.read("#{@static_files_path}/database.yml").gsub('db_name', @application_name.downcase)
+  file 'config/database.yml.example', open("#{@static_files_path}/database.yml").read.gsub('db_name', @application_name.downcase)
+  file 'config/database.yml', open("#{@static_files_path}/database.yml").read.gsub('db_name', @application_name.downcase)
 end
