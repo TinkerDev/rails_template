50968d80cd8557f86aa90d5b52dfcb111b4cb2d0
Alexander Logunov <unlovedru@gmail.com>
2015-04-06 03:52:05 +0300

    cap

--- recipes/_gems.rb
+++ recipes/_gems.rb
@@ -2,7 +2,12 @@ def recipe_gems
   run "rm Gemfile.lock"
   copy_application_file('Gemfile')
 
+  prepare_secret_tokens
   prepare_carrierwave
+end
+
+def prepare_secret_tokens
+  file 'config/initializers/secret_token.rb', open(application_file_path("config/initializers/secret_token.rb")).read.gsub('app_name', app_name.capitalize)
 end
 
 def prepare_carrierwave
