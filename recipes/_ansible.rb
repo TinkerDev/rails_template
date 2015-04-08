a011080c11bbc89312b60227fe5b73dab98e428d
Alexander Logunov <unlovedru@gmail.com>
2015-04-08 16:26:56 +0300

    ansible fix

--- recipes/_ansible.rb
+++ recipes/_ansible.rb
@@ -2,5 +2,4 @@ def recipe_ansible
   file 'ansible/playbook.yml', open(application_file_path("ansible/playbook.yml")).read.gsub('app_name', app_name.downcase)
   copy_application_file('ansible/configs/database.yml')
   copy_application_file('ansible/configs/nginx.conf')
-  copy_application_file('ansible/configs/pg_hba.conf')
 end
